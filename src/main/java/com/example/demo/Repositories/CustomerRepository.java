ced8681181721bccd3a87a4fe1dcefa8654ab917
bondan <bondanocho123@yahoo.com>
2018-12-25 23:02:53 +0700

    customer

--- src/main/java/com/example/demo/Repositories/CustomerRepository.java
+++ src/main/java/com/example/demo/Repositories/CustomerRepository.java
@@ -2,10 +2,17 @@ package com.example.demo.Repositories;
 
 import com.example.demo.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 
+    @Query("SELECT c FROM tbl_customer c WHERE c.id = :id")
+    Customer findCustomerById(@Param("id") Integer id) ;
+
+    @Query("SELECT c FROM tbl_customer c WHERE c.id = :nama_customer")
+    Customer  findCustomerName(@Param("nama_customer") String customerName) ;
 
 }
--- src/main/java/com/example/demo/Services/CustomerService.java
+++ src/main/java/com/example/demo/Services/CustomerService.java
@@ -1,13 +1,50 @@
 package com.example.demo.Services;
 
+
+import com.example.demo.Repositories.CustomerRepository;
+import com.example.demo.model.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
 public class CustomerService {
 
     @Autowired
+    CustomerRepository customerRepository;
+
+    public List<Customer> findAll()
+    {
+        return customerRepository.findAll();
+    }
+
+    public Customer customerSave(Customer customer)
+    {
+        return customerRepository.save(customer);
+    }
+
+    public Customer findCustomerById(Integer id)
+    {
+        return customerRepository.findCustomerById(id)
+    }
+
+    public Customer findCustomerByName(String customerName)
+    {
+        return customerRepository.findCustomerName(customerName);
+    }
+
+    public void deleteCustomer(Integer Id) {
+        Customer customer = customerRepository.findCustomerById(Id);
+        try {
+            customerRepository.delete(customer);
+        }
+        catch (Exception exception)
+        {
+            System.out.print(exception.getMessage());
+        }
+    }
 
 }
